1245a852a0e15a272c2b0ded38c47794aab65306
rloterh <rloterh@gmail.com>
2021-06-13 02:01:55 +0000

    Create an element component for variables

---
+++ src/components/elements.js
@@ -0,0 +1,23 @@
+const city = document.querySelector('.city');
+const weatherIcon = document.querySelector('.icon');
+const weatherDescription = document.querySelector('.description');
+const temperature = document.querySelector('.temp');
+const wind = document.querySelector('.wind');
+const weather = document.querySelector('.weather');
+const body = document.body;
+const searchBar = document.querySelector('.search-bar');
+const searchButton = document.querySelector('.search button');
+const humidity = document.querySelector('.humidity');
+
+export {
+  city,
+  weatherIcon,
+  weatherDescription,
+  temperature,
+  wind,
+  weather,
+  body,
+  searchBar,
+  searchButton,
+  humidity
+};
